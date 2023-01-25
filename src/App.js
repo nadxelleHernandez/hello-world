836e47c60750417de573312fd5dba3a6bb6e4a98
nadxelle <nadxellehernandez@gmail.com>
2023-01-24 17:08:01 -0800

    Create first Components

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
-import logo from "./logo.svg";
-import "./App.css";
+import StudentList from "./components/StudentList";
+import ClassInfo from "./components/ClassInfo";
 
 function App() {
-  const myName = "Nadxelle";
+  //const myName = "Nadxelle";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React hello
-        </a>
-      </header>
-      <section>
-        <h2 className="App-link">Hello World</h2>
-        <h3>{myName}</h3>
-      </section>
-    </div>
+    <main>
+      <h1>Attendance</h1>
+      <ClassInfo></ClassInfo>
+      <StudentList></StudentList>
+    </main>
   );
 }
 
---
+++ src/components/StudentList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const StudentList = () => {
+  return <h2>Student List</h2>;
+};
+
+export default StudentList;
