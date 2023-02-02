09becaba79ea7a1119e7fdbfd2e056187ef1c804
nadxelle <nadxellehernandez@gmail.com>
2023-02-02 14:53:12 -0800

    create form component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import StudentList from "./components/StudentList";
 import ClassInfo from "./components/ClassInfo";
 import { useState } from "react";
+import NewStudentForm from "./components/NewStudentForm";
 
 function App() {
   const [studentData, setStudentData] = useState([
@@ -39,6 +40,23 @@ function App() {
     setStudentData([]);
   };
 
+  const addStudentData = (newStudent) => {
+    // Duplicate the student list
+    const newStudentList = [...studentData];
+
+    // Logic to generate the next valid student ID
+    const nextId = Math.max(...newStudentList.map((student) => student.id)) + 1;
+
+    newStudentList.push({
+      id: nextId,
+      nameData: newStudent.nameData,
+      emailData: newStudent.emailData,
+      isPresentData: false,
+    });
+
+    setStudentData(newStudentList);
+  };
+
   return (
     <main>
       <h1>Attendance</h1>
@@ -50,6 +68,9 @@ function App() {
         students={studentData}
         onUpdateStudent={updateStudentData}
       ></StudentList>
+      <section>
+        <NewStudentForm addStudentCallback={addStudentData}></NewStudentForm>
+      </section>
     </main>
   );
 }
